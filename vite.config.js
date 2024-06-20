29fe2b850572a0356b17b5280b52996843bf1f0e
Rhamseys Garcia <rhamseysgarcia@MacBook-Pro.lan>
2024-06-20 15:36:52 -0700

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default {
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 500,
     rollupOptions: {
       input: {
         main: 'client/index.html',
       },
     },
   },
-}
+})
